20cb667892372338c312061a2e1ad01e86ccc284
Avvi Potarlanka <avvipotarlanka@Rams-MacBook-Air.local>
2026-02-20 00:31:03 -0500

    day2 comment

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod solutions;
 pub fn solve_day(day: u8) {
     match day {
         1 => solutions::day01::solve(),
+	2 => println!("Day 02 to be built"),
         _ => println!("Day {} not built yet", day),
     }
 
